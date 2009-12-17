71ddf6743908b42d64f75b32ab390effad05f862
Ameravant <contact@ameravant.com>
2009-12-17 15:10:29 -0800

    Fixed step 3 issues

--- step_2.rb
+++ step_2.rb
@@ -1,7 +1,9 @@
 setup = YAML::load_file("#{RAILS_ROOT}/config/cms.yml")
+inside('vendor/plugins/siteninja') do
   run "svn co http://66.103.153.150:86/svn/repository/siteninja/vendor/plugins/siteninja/siteninja_documents" if setup['modules']['documents']
   run "svn co http://66.103.153.150:86/svn/repository/siteninja/vendor/plugins/siteninja/siteninja_events" if setup['modules']['events']
   run "svn co http://66.103.153.150:86/svn/repository/siteninja/vendor/plugins/siteninja/siteninja_newsletters" if setup['modules']['newsletters']
   run "svn co http://66.103.153.150:86/svn/repository/siteninja/vendor/plugins/siteninja/siteninja_store" if setup['modules']['product']
   run "svn co http://66.103.153.150:86/svn/repository/siteninja/vendor/plugins/siteninja/siteninja_galleries" if setup['modules']['galleries']
   run "svn co http://66.103.153.150:86/svn/repository/siteninja/vendor/plugins/siteninja/siteninja_links" if setup['modules']['links']
+end
--- step_3.rb
+++ step_3.rb
@@ -3,13 +3,14 @@ inside('vendor/plugins/siteninja/siteninja_setup') do
   run "echo Copying default application layout, stylesheets and configuration files..."
   run "mv application.html.haml #{RAILS_ROOT}/app/views/layouts"
   run "mv application_controller.rb #{RAILS_ROOT}/app/controllers"
-  run "mv #{RAILS_ROOTS}/config/routes.rb #{RAILS_ROOTS}/config/routes-setup-backup.rb"
+  run "mv #{RAILS_ROOT}/config/routes.rb #{RAILS_ROOT}/config/routes-setup-backup.rb"
   run "mv routes.rb #{RAILS_ROOT}/config"
   run "mv permissions.yml #{RAILS_ROOT}/config"
   run "mv environment.rb #{RAILS_ROOT}/config"
   run "mv production.rb #{RAILS_ROOT}/config/environments"
   run "mv *.css #{RAILS_ROOT}/public/stylesheets"
 end
+
 run "script/generate plugin_migration"
 if setup['website']['environment'] == "production"
   run "rake db:drop db:create db:migrate db:populate_min RAILS_ENV=production"
