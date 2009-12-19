a590653feed0e6aa18e7ed3fa60c0af812ddc84d
Ameravant <contact@ameravant.com>
2009-12-18 16:24:05 -0800

    fixed git urls

--- step_1.rb
+++ step_1.rb
@@ -1,10 +1,10 @@
 run "rm public/index.html"
 inside('vendor/plugins/siteninja') do
-  run "get clone git://github.com/ameravant/siteninja_pages.git"
-  run "get clone git://github.com/ameravant/siteninja_blog.git"
-  run "get clone git://github.com/ameravant/siteninja_core.git"
-  run "get clone git://github.com/ameravant/siteninja_setup.git"
+  run "git clone git://github.com/ameravant/siteninja_pages.git"
+  run "git clone git://github.com/ameravant/siteninja_blog.git"
+  run "git clone git://github.com/ameravant/siteninja_core.git"
+  run "git clone git://github.com/ameravant/siteninja_setup.git"
 end
 inside('vendor/plugins') do
-  run "get clone git://github.com/ameravant/siteninja_plugins.git"
+  run "git clone git://github.com/ameravant/siteninja_plugins.git"
 end
