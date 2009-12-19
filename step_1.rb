23caa0a5d1b013d7153dc4332e0d0a47498e499e
Ameravant <contact@ameravant.com>
2009-12-18 16:29:02 -0800

    fixing plugins

--- step_1.rb
+++ step_1.rb
@@ -1,7 +1,7 @@
 run "rm public/index.html"
 inside('vendor/plugins/siteninja') do
   run "git clone git://github.com/ameravant/siteninja_pages.git"
-  run "git clone git://github.com/ameravant/siteninja_blog.git"
+  run "git clone git://github.com/ameravant/siteninja_blogs.git"
   run "git clone git://github.com/ameravant/siteninja_core.git"
   run "git clone git://github.com/ameravant/siteninja_setup.git"
 end
