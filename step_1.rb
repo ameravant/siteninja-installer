ca568ed5ea9565d412ff18c2b3edf3d6f8822357
Ameravant <contact@ameravant.com>
2009-12-18 18:05:45 -0800

    fixing will_paginate

--- step_1.rb
+++ step_1.rb
@@ -9,3 +9,4 @@ inside('vendor/plugins') do
   run "git clone git://github.com/ameravant/siteninja_plugins.git"
   run "mv siteninja_plugins/* siteninja/"
 end
+run "script/plugin install git://github.com/mislav/will_paginate.git"
