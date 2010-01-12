239ff43711e978d23e528c2a901682ad17474a9d
Ameravant <contact@ameravant.com>
2010-01-12 11:52:27 -0800

    Adding 301 redirect

--- step_2.rb
+++ step_2.rb
@@ -7,3 +7,9 @@ inside('vendor/plugins/siteninja') do
   run "git clone git@github.com:ameravant/siteninja_galleries.git" if setup['modules']['galleries']
   run "git clone git@github.com:ameravant/siteninja_links.git" if setup['modules']['links']
 end
+run "echo 'Options +FollowSymLinks ' >> #{RAILS_ROOT}/public/.htaccess"
+run "echo 'RewriteEngine on ' >> #{RAILS_ROOT}/public/.htaccess"
+run "echo 'RewriteCond %{HTTP_HOST} ' >> #{RAILS_ROOT}/public/.htaccess"
+run "echo 'RewriteCond %{HTTP_HOST} ^#{setup['website']['domain']} ' >> #{RAILS_ROOT}/public/.htaccess"
+run "echo 'RewriteRule (.*) http://www.#{setup['website']['domain']}/$1 [R=301,L] ' >> #{RAILS_ROOT}/public/.htaccess"
+
