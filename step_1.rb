844cae2b082df941d5d068e77ee01a6b471b7ae2
Jenn Standard <jennstandard@jennifer-dedoess-macbook.local>
2010-01-21 13:19:56 -0800

    telling app to switch to s3 branches of other modules

--- step_1.rb
+++ step_1.rb
@@ -5,6 +5,15 @@ inside('vendor/plugins/siteninja') do
   run "git clone git@github.com:ameravant/siteninja_core.git"
   run "git clone git@github.com:ameravant/siteninja_setup.git"
 end
+
+############### These lines need to be removed after merging s3 branch into master #########
+inside('vendor/plugins/siteninja/siteninja_core') do
+  run "git checkout s3_branch"
+end
+inside('vendor/plugins/siteninja/siteninja_setup') do
+  run "git checkout s3_branch"
+end
+###################################################
 inside('vendor/plugins') do
   run "git clone git@github.com:ameravant/siteninja_plugins.git"
   run "mv siteninja_plugins/* siteninja/"
